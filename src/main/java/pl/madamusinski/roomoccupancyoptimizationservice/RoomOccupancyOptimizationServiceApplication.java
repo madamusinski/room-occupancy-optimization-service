4c755124fcc40732d1d3c560e54ffa3da1eef6af
Mateusz Adamusinski <adamusinskimateusz@gmail.com>
2023-11-26 15:41:28 +0100

    initial commit

---
+++ src/main/java/pl/madamusinski/roomoccupancyoptimizationservice/RoomOccupancyOptimizationServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.madamusinski.roomoccupancyoptimizationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomOccupancyOptimizationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomOccupancyOptimizationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/madamusinski/roomoccupancyoptimizationservice/RoomOccupancyOptimizationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.madamusinski.roomoccupancyoptimizationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomOccupancyOptimizationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
